b2d291eaa3f7d0c56a68b16d1677350ad7eba6a0
안현 <165144753+slyhyun@users.noreply.github.com>
2026-09-03 13:40:14 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 73 ms, Memory: 25,600 KB -BaekjoonHub

--- slyhyun/SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ slyhyun/SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,9 +1,9 @@
-import java.util.*;
 import java.io.*;
 
 public class Solution {
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
+	static int result;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,16 +13,13 @@ public class Solution {
 			br.readLine();
 			
 			int[][] maze = new int[16][16];
-			
 			int r = -1;
 			int c = -1;
 			
 			for (int i = 0; i < 16; i++) {
                 String line = br.readLine();
-                
                 for (int j = 0; j < 16; j++) {
                     maze[i][j] = line.charAt(j) - '0';
-                    
                     if (maze[i][j] == 2) {
                         r = i;
                         c = j;
@@ -30,7 +27,11 @@ public class Solution {
                 }
             }
 			
-			int result = bfs(r, c, maze);
+			maze[r][c] = 1;
+			
+			result = 0;
+			
+			dfs(r, c, maze);
 			
 			sb.append("#").append(tc).append(" ").append(result).append("\n");
 		}
@@ -38,36 +39,25 @@ public class Solution {
 		System.out.print(sb);
 	}
 	
-	static int bfs(int startR, int startC, int[][] maze) {
-		Queue<int[]> q = new ArrayDeque<>();
-		
-		maze[startR][startC] = 1;
-		q.offer(new int[]{startR, startC});
-		
-		while (!q.isEmpty()) {
-			int[] curr = q.poll();
-			
-			int r = curr[0];
-			int c = curr[1];
-			
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if (maze[nr][nc] == 3) {
-					return 1;
-				}
-				
-				if (maze[nr][nc] == 1) {
-					continue;
-				}
-
-				maze[nr][nc] = 1;
-				q.offer(new int[]{nr, nc});
-			}
-		}
-		
-		return 0;
+	static void dfs(int r, int c, int[][] maze) {
+	    if (result == 1) return;
+	    
+	    if (maze[r][c] == 3) {
+	        result = 1;
+	        
+	        return;
+	    }
+	    
+	    maze[r][c] = 1;
+	    
+	    for (int d = 0; d < 4; d++) {
+	        int nr = r + dr[d];
+	        int nc = c + dc[d];
+	        
+	        if (maze[nr][nc] == 1) continue;
+	        
+	        dfs(nr, nc, maze);
+	    }
 	}
 
 }
